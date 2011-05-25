2a848179cdc5282978cb338be052ab5b6e575ff9
Matthew Henderson <matthew.james.henderson@gmail.com>
2011-05-25 16:59:38 +0100

    Output of latin designs updated.

--- ryser/designs.py
+++ ryser/designs.py
@@ -1,16 +1,16 @@
-# Matthew Henderson, 18.12.2010 (Chandlers Ford)
+# Matthew Henderson, 2010.12.18 (Chandlers Ford)
 
-from output import dict_to_string
+from output import dict_to_string_xxx
 
-class latin():
-    """This is a component for building latin square objects."""
+class Latin:
+    """This is a component for building (partial) latin rectangle objects."""
     def __init__(self, fixed, size):
         self.__nrows__ = size[0]
         self.__ncols__ = size[1]
         self.__fixed__ = fixed
 
     def __repr__(self):
-        return dict_to_string(self.__fixed__, self.__nrows__, self.__ncols__, padding = 1, rowend = "\n")
+        return dict_to_string_xxx(self.__fixed__, self.__nrows__, self.__ncols__)
 
     def __call__(self, row, column):
         return self.__fixed__.get((row, column))
@@ -21,8 +21,8 @@ class latin():
     def ncols(self):
         return self.__ncols__
 
-class gerechte():
+class Gerechte:
     pass
 
-class sudoku():
+class Sudoku:
     pass
--- ryser/output.py
+++ ryser/output.py
@@ -1,4 +1,15 @@
-def dict_to_string(fixed, nrows, ncols, padding = 0, row_sep = '', col_sep = ''):
+def f(fixed, col_sep, cols, row, row_sep, padding):
+    s = col_sep
+    for col in cols:
+        symbol = fixed.get((row, col))
+        if symbol:
+            s += ' '*padding + symbol
+        else:
+            s += ' '*padding + '.'   
+        s += col_sep
+    return s
+
+def dict_to_string(fixed, rows, cols, padding = 0, row_sep = '', col_sep = '', top = '', bottom = ''):
     """
     Returns a puzzle string of dimension 'boxsize' from a dictionary of 
     'fixed' cells.
@@ -8,23 +19,24 @@ def dict_to_string(fixed, nrows, ncols, padding = 0, row_sep = '', col_sep = '')
     col_sep : a string added to the last symbol of every column
     
     """
-    s = row_sep
-    for row in range(nrows):
-        s += col_sep
-        for col in range(ncols):
-            symbol = fixed.get((row, col))
-            if symbol:
-                s += ' '*padding + symbol
-            else:
-                s += ' '*padding + '.'   
-            s += col_sep
+    s = top
+    for row in rows[:-1]:
+        s += f(fixed, col_sep, cols, row, row_sep, padding)
         s += row_sep
+    s += f(fixed, col_sep, cols, rows[-1], row_sep, padding)
     s += ' '*padding
+    s += bottom
     return s
 
-def dict_to_string_std(fixed, nrows, ncols, padding = 0):
+def dict_to_string_std(fixed, rows, cols, padding = 0):
     """Returns a puzzle string of dimension 'boxsize' from a dictionary of 
     'fixed' cells with some suitably chosen row/column seperators."""
+    ncols = len(cols)
     row_sep = '\n' + (2*ncols + 1)*'-' + '\n'
     col_sep = '|'
-    return dict_to_string(fixed, nrows, ncols, padding, row_sep, col_sep)
+    return dict_to_string(fixed, rows, cols, padding, row_sep, col_sep, row_sep, row_sep)
+
+def dict_to_string_xxx(fixed, nrows, ncols, padding = 0):
+    rows = range(nrows)
+    cols = range(ncols)
+    return dict_to_string_std(fixed, rows, cols, padding)
