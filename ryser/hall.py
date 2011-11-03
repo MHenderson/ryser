70d416062995d5e05d8948b99bc3c95bb6bcb7d8
Matthew Henderson <matthew.james.henderson@gmail.com>
2011-11-03 22:47:30 +0000

    Implementation of Hall numbers function.

--- ryser/hall.py
+++ ryser/hall.py
@@ -1,4 +1,5 @@
 from vizing.hall import hall_inequality_induced_by
+from vizing.hall import hall_number
 
 from ryser.graphs import latin_graph, symmetric_latin_graph
 from ryser.utils import list_assignment, vertex 
@@ -23,3 +24,15 @@ def symmetric_hall_inequality_on_cells(partial_latin_square, size, cells):
     graph = symmetric_latin_graph(size)
     lists = list_assignment(partial_latin_square, size)
     return hall_inequality_on_cells_g(graph, lists, size, cells)   
+
+def hall_numbers(partial_latin_square, size, cells):
+    """Returns a list of Hall numbers for the subgraph induced by cells in
+    cells.
+    TODO: Implement hall_numbers in vizing and then rewrite this function."""
+    cell_vertices = [vertex(x, size) for x in cells]
+    H = symmetric_latin_graph(size).subgraph(cell_vertices)
+    L = list_assignment(partial_latin_square, size)
+    colours = range(1, size + 1)
+    hall_numbers = [hall_number(H, L, c) for c in colours]
+    return hall_numbers
+
