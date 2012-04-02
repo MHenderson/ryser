58d69d469903e16ecdccd329bbd20757824c8196
Matthew Henderson <matthew.james.henderson@gmail.com>
2012-04-02 19:19:49 +0100

    More examples and further testing of Hall components.

--- tests/hall_tests.py
+++ tests/hall_tests.py
@@ -2,8 +2,8 @@
 
 import unittest
 
-from ryser.examples import eg1, eg2
-from ryser.examples import fail1, fail2, fail3
+from ryser.examples import eg1, eg2, eg3, eg4
+from ryser.examples import fail1, fail2, fail3, fail4
 
 from ryser.hall import hall_inequality_on_cells
 from ryser.hall import symmetric_hall_inequality_on_cells
@@ -33,6 +33,8 @@ class TestHallConditionOnCells(unittest.TestCase):
         self.__assert_false_hall_inequality_on_example(eg1, fail1)
         self.__assert_false_hall_inequality_on_example(eg2, fail2)
         self.__assert_true_hall_inequality_on_example(eg2, fail3)
+        self.__assert_true_hall_inequality_on_example(eg3, fail4)
+        self.__assert_true_hall_inequality_on_example(eg4, fail4)
 
 class TestSymmetricHallConditionOnCells(unittest.TestCase):
 
@@ -60,6 +62,8 @@ class TestSymmetricHallConditionOnCells(unittest.TestCase):
         self.__assert_true_symmetric_hall_inequality_on_example(eg1, fail1)
         self.__assert_false_symmetric_hall_inequality_on_example(eg2, fail2)
         self.__assert_false_symmetric_hall_inequality_on_example(eg2, fail3)
+        self.__assert_false_symmetric_hall_inequality_on_example(eg3, fail4)
+        self.__assert_false_symmetric_hall_inequality_on_example(eg4, fail4)
 
 if __name__ == '__main__':
     unittest.main()
