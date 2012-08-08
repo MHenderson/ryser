dc80973654b2d7c6b472f631d977858438ff7d64
Matthew Henderson <matthew.james.henderson@gmail.com>
2012-08-08 15:40:15 +0100

    Readme, License and distutils setup config file created.

---
+++ README.txt
@@ -0,0 +1,5 @@
+# Matthew Henderson, 2012
+# Created Wed Aug  8 15:39:10 BST 2012. Last updated Wed Aug  8 15:39:20 BST 2012
+
+Welcome to the Ryser package for Python.
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+# Matthew Henderson, 2012
+# Created Wed Aug  8 15:37:51 BST 2012. Last updated Wed Aug  8 15:38:03 BST 2012
+
+from distutils.core import setup
+
+setup(
+    name = 'Ryser',
+    version = '0.1dev',
+    packages = ['ryser',],
+    license = ' Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License',
+    long_description = open('README.txt').read(),
+)
+
