553096024adf079a410ad277d2549f6995ed2341
Matthew Henderson <matthew.james.henderson@gmail.com>
2012-08-14 13:44:13 +0100

    An important correction and removal of cluster script.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name = 'ryser',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = ['ryser',],
     description = "Latin squares and related designs.",
     author = "Matthew Henderson",
