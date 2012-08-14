5b29726feb91beea24051cdebbd54380dd29bc34
Matthew Henderson <matthew.james.henderson@gmail.com>
2012-08-14 10:26:13 +0100

    Correction to script for testing Hilton's claim.

--- investigations/hiltons_claim.sh
+++ investigations/hiltons_claim.sh
@@ -2,22 +2,18 @@
 
 a=1
 b=$((2**34+1))
-k=$1
+k=$SGE_TASK_LAST
 d=$((b-a))
 s=$((d/k))
 j=$SGE_TASK_ID
 
-echo this is a line of output # except for this
-echo $a
-echo $b
-echo $k
-echo $s
-echo $j 
+#echo $a
+#echo $b
+#echo $k
+#echo $s
+#echo $j
 
-command="program_name"
+command="is_hall_on_interval"
 
-END=$((k-1))
-for ((i=0;i<=END;i++));
-do
-   echo $command $((a+i*s)) $((a+(i+1)*s-1))
-done
+echo $command $((a+(j-1)*s)) $((a+(j)*s-1))
+
