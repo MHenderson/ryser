ad66eeef12edb263bcaf76bd3eef609a9f112f4a
Matthew Henderson <matthew.james.henderson@gmail.com>
2013-03-26 10:35:54 +0000

    Completion algorithm created.

--- ryser/completion.py
+++ ryser/completion.py
@@ -1,6 +1,6 @@
 # Copyright Matthew Henderson 2013.
 # Created Tue Mar 26 10:02:38 GMT 2013
-# Last updated: Tue Mar 26 10:10:48 GMT 2013
+# Last updated: Tue Mar 26 10:35:25 GMT 2013
 
 def extend(P, w, t, e):
     """
@@ -16,4 +16,11 @@ def extend(P, w, t, e):
         return {7:6,8:8,15:8,16:7,23:2,24:3,31:1,32:2,39:7,40:4,47:4,\
                 48:1,49:6,50:8,51:2,52:1,53:7,54:4,55:3,57:8,58:7,59:3,\
                 60:2,61:4,62:1,64:6}
+
+def complete(P, w, t, e):
+    if w==P.size():
+        return P
+    else:
+        P.extend(extend(P, w, t, e))
+        return complete(P, w + 2, t - 2, e)
 
